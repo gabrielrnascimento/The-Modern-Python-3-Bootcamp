36b42a0c6b1725898a7bd789feaac0e3eddc6131
gabrielrnascimento <59718237+gabrielrnascimento@users.noreply.github.com>
2020-07-04 01:48:29 -0300

    Update API_request_jokes.py

--- API_request_jokes.py
+++ API_request_jokes.py
@@ -1,30 +1,44 @@
 # Asks for a theme and get jokes about this theme on https://icanhazdadjoke.com
 
 import requests
+from termcolor import colored, cprint
+import colorama
+import pyfiglet
 
 url = "https://icanhazdadjoke.com/search"
 
+def header():
+    colorama.init()
+    header = colored(pyfiglet.figlet_format("DAD JOKES inc."), color="green")
+    return print(header)
+
+header()
+
 joke_theme = input("Choose the joke theme: ")
 response = requests.get(
-    url, 
+    url,
     headers={"Accept": "application/json"},
     params={"term": joke_theme}
-    )
+).json()
 
-
-data = response.json()
-
-print(f"We have {data['total_jokes']} {joke_theme} joke(s), here's one: ")
-print(data['results'][0]['joke'])
-count = 0
-count += 1
-while count < len(data['results']):
-    ask_another = input("Do you want another joke? (y/n) ")
-    if ask_another.lower() == 'y':
-        print(data['results'][count]['joke'])
-        count += 1
+if response['total_jokes'] >= 1:
+    if response['total_jokes'] == 1:
+        print(f"I've got {response['total_jokes']} {joke_theme} joke, here it is: ")
+        print(response['results'][0]['joke'])
     else:
-        print("No more jokes for you")
-        break
+        print(f"I've got {response['total_jokes']} {joke_theme} jokes, here's one: ")
+        print(response['results'][0]['joke'])
+        count = 0
+        count += 1
+        while count < len(response['results']):
+            ask_another = input("Do you want another joke? (y/n) ")
+            if ask_another.lower() == 'y':
+                print(response['results'][count]['joke'])
+                count += 1
+            else:
+                print("No more jokes for you")
+                break
+        else:
+            print(f"There are no more {joke_theme} jokes")
 else:
-    print(f"There are no more {joke_theme} jokes")
+    print(f"Sorry, I don't have any jokes about {joke_theme}. Please try again.")

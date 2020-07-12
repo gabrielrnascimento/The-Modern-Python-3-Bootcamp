a44d854f98043710d7457a87d28cc9c612e0c4f1
gabrielrnascimento <59718237+gabrielrnascimento@users.noreply.github.com>
2020-07-12 18:35:24 -0300

    Update tic_tac_toe.py
    randomness which player or computer does the first move
    wins counter

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,32 +1,40 @@
+from random import choice
 
-board = []
-for x in range(10):
-    board.append(" ")
+player_wins = 0
+computer_wins = 0
+
+board = [" " for x in range(10)]
+
 
 def printTitle():
     print()
     print("Welcome to Tic Tac Toe Game!".upper())
     print()
-    
+
+
 def clean_board(board):
     for i in range(10):
         board[i] = " "
     return board
 
 # Checks if there is no more ' ' space to make a move
+
+
 def isBoardFull(board):
     if board.count(" ") > 1:
         return False
     return True
 
 # Prints the actual board with the moves
+
+
 def printBoard(board):
     print("------------------------------------")
-    print(f"|     {board[1]}     |    {board[2]}     |     {board[3]}     |")
+    print(f"|     {board[7]}     |    {board[8]}     |     {board[9]}     |")
     print("------------------------------------")
     print(f"|     {board[4]}     |    {board[5]}     |     {board[6]}     |")
     print("------------------------------------")
-    print(f"|     {board[7]}     |    {board[8]}     |     {board[9]}     |")
+    print(f"|     {board[1]}     |    {board[2]}     |     {board[3]}     |")
     print("------------------------------------")
 
 
@@ -35,30 +43,33 @@ def validMove(board, move):
     if board[move] == " ":
         return True
     return False
-    
+
 
 # Return the player choice between 1-9
 def playerMove(board):
-    while True:
-        move = int(input("Choose a board square to play (1-9): "))
-        if move in list(range(1,10)):
-            if validMove(board, move):
-                board[move] = 'X'
-                return printBoard(board)
+    run = True
+    while run:
+        move = input("Choose a board square to play (1-9): ")
+        try:
+            move = int(move)
+            if move in list(range(1, 10)):
+                if validMove(board, move):
+                    run = False
+                    board[move] = 'X'
+                    return printBoard(board)
+                else:
+                    print("This board square is already occupied")
             else:
-                print("This board square is already occupied")
-        else:
-            print("Choose a number between 1 and 9")
-# STILL NEED FIXING NOT INT INPUT <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-
+                print("Choose a number between 1 and 9")
+        except:
+            print("Please type a number")
 
 
 # Return the computer move based on an algorithm
 def compMove(board):
-    from random import choice
 
     # 1. computer win chance
-    for i in range(1,10):
+    for i in range(1, 10):
         move = i
         if validMove(board, move):
             board[i] = "O"
@@ -68,7 +79,7 @@ def compMove(board):
                 board[i] = " "
 
     # 2. player win chance (block player)
-    for i in range(1,10):
+    for i in range(1, 10):
         move = i
         if validMove(board, move):
             board[i] = "X"
@@ -92,48 +103,87 @@ def compMove(board):
 
     # 5. random available place
     while True:
-        move = choice(list(range(1,10)))
+        move = choice(list(range(1, 10)))
         if validMove(board, move):
             board[move] = "O"
             return printBoard(board)
 
-    """
-    algorithm
-        1. computer win chance
-        2. player win chance (block player)
-        3. available corners of the board
-        4. center of the board (if available)
-        5. random available place
-    """
-
 # Checks if whether "O" or "X" wins the game
+
+
 def isWinner(board, letter):
-    if  (board[1] == letter and board[2] == letter and board[3] == letter) or (board[4] == letter and board[5] == letter and board[6] == letter) or (board[7] == letter and board[8] == letter and board[9] == letter) or (board[1] == letter and board[4] == letter and board[7] == letter) or (board[2] == letter and board[5] == letter and board[8] == letter) or (board[3] == letter and board[6] == letter and board[9] == letter) or (board[1] == letter and board[5] == letter and board[9] == letter) or (board[3] == letter and board[5] == letter and board[7] == letter):
+    if (board[1] == letter and board[2] == letter and board[3] == letter) or (board[4] == letter and board[5] == letter and board[6] == letter) or (board[7] == letter and board[8] == letter and board[9] == letter) or (board[1] == letter and board[4] == letter and board[7] == letter) or (board[2] == letter and board[5] == letter and board[8] == letter) or (board[3] == letter and board[6] == letter and board[9] == letter) or (board[1] == letter and board[5] == letter and board[9] == letter) or (board[3] == letter and board[5] == letter and board[7] == letter):
         return True
     return False
 
+# Starts the game and the player does the first move
+
+
+def main_player():
+    global computer_wins
+    global player_wins
+    print("The player ('X') starts!")
+    printBoard(board)
+    while True:
+        playerMove(board)
+        if isWinner(board, "X"):
+            print("X wins")
+            player_wins += 1
+            break
+        if isBoardFull(board):
+            print("It's a Tie!")
+            break
+        compMove(board)
+        if isWinner(board, "O"):
+            print("O wins")
+            computer_wins += 1
+            break
+        if isBoardFull(board):
+            print("It's a Tie!")
+            break
+
+# Starts the game and the computer does the first move
+
+
+def main_comp():
+    global computer_wins
+    global player_wins
+    print("The computer ('O') starts!")
+    while True:
+        compMove(board)
+        if isWinner(board, "O"):
+            print("O wins")
+            computer_wins += 1
+            break
+        if isBoardFull(board):
+            print("It's a Tie!")
+            break
+        playerMove(board)
+        if isWinner(board, "X"):
+            print("X wins")
+            player_wins += 1
+            break
+        if isBoardFull(board):
+            print("It's a Tie!")
+            break
+
+
 def main():
+    first_move = {1: main_player, 2: main_comp}
     replay_bool = True
     while replay_bool:
         printTitle()
         clean_board(board)
-        printBoard(board)
-        while not isWinner(board, "X") and not isWinner(board, "O"):
-            playerMove(board)
-            if isBoardFull(board):
-                break
-            compMove(board)
-        if isWinner(board, "X"):
-            print("X wins")
-        elif isWinner(board, "O"):
-            print("O wins")
-        elif isBoardFull(board):
-            print("Tie!")
+        first_move_choice = choice([1, 2])
+        first_move.get(first_move_choice)()
+        print(f"player wins = {player_wins}")
+        print(f"computer wins = {computer_wins}")
         replay = input("Do you want to play another round? (y/n) ")
         if replay == "y":
             replay_bool = True
         else:
             replay_bool = False
     print("Thank you for playing!")
+
 
 main()
